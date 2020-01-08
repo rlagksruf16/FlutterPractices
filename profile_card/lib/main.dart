eec2df86f9dbb0247f2dab6974f895d737005fb7
rlagksruf16 <rlagksruf16@gmail.com>
2020-01-08 21:54:01 +0900

    personalize prof_card

--- profile_card/lib/main.dart
+++ profile_card/lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.lightBlue[400],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -34,60 +35,66 @@ class MyApp extends StatelessWidget {
                 'DSC HUFS Lead',
                 style: TextStyle(
                   fontFamily: 'SourceSansPro',
-                  color: Colors.teal[100],
+                  color: Colors.lightBlue[100],
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
-                )
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        '+82 10-5046-4216',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 15.0,
-                        ),
-                      
-                      ),
-                  ],
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.lightBlue.shade100,
+                )
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.lightBlue[400],
+                    ),
+                  title: Text(
+                    '+82 10-5046-4216',
+                    style: TextStyle(
+                      color: Colors.lightBlue.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+                child: ListTile(
+                  leading: Icon(
                       Icons.mail,
-                      color: Colors.teal,
+                      color: Colors.lightBlue[400],
                       ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
+                      title: Text(
                         'rlagksruf16@gmail.com',
                         style: TextStyle(
-                          color: Colors.teal.shade900,
+                          color: Colors.lightBlue.shade900,
                           fontFamily: 'SourceSansPro',
                           fontSize: 15.0,
                         ),
                       ),
-                  ],
-                )
-              )
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
+                child: ListTile(
+                      title: Text(
+                        'Click Me',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.lightBlue.shade900,
+                          fontFamily: 'PressStart2P',
+                          fontSize: 15.0,
+                        ),
+                      ),
+                ),
+              ),
             ],
           )
         ),
@@ -95,3 +102,65 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+// Container 활용한 카드 만들기
+
+
+// Container(
+//                 padding: EdgeInsets.all(10.0),
+//                 color: Colors.white,
+//                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                 child: Row(
+//                   children: <Widget>[
+//                     Icon(
+//                       Icons.phone,
+//                       color: Colors.teal,
+//                       ),
+//                       SizedBox(
+//                         width: 10.0,
+//                       ),
+//                       Text(
+//                         '+82 10-5046-4216',
+//                         style: TextStyle(
+//                           color: Colors.teal.shade900,
+//                           fontFamily: 'SourceSansPro',
+//                           fontSize: 15.0,
+//                         ),
+                      
+//                       ),
+//                   ],
+//                 ),
+//               ),
+//               Container(
+//                 padding: EdgeInsets.all(10.0),
+//                 color: Colors.white,
+//                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                 child: Row(
+//                   children: <Widget>[
+//                     Icon(
+//                       Icons.mail,
+//                       color: Colors.teal,
+//                       ),
+//                       SizedBox(
+//                         width: 10.0,
+//                       ),
+//                       Text(
+//                         'rlagksruf16@gmail.com',
+//                         style: TextStyle(
+//                           color: Colors.teal.shade900,
+//                           fontFamily: 'SourceSansPro',
+//                           fontSize: 15.0,
+//                         ),
+//                       ),
+//                   ],
+//                 )
+//               )
