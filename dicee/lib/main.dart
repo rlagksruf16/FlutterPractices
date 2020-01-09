61238658de7698a44e76db863b62ab599dcba406
rlagksruf16 <rlagksruf16@gmail.com>
2020-01-10 00:39:06 +0900

    minimalize codes using function

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -26,6 +26,12 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 5;
   int rightDiceNumber = 1;
 
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,9 +40,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6)+1;
-                });
+                changeDiceFace();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -45,7 +49,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  rightDiceNumber = Random().nextInt(6)+1;
+                  changeDiceFace();
                 });
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
