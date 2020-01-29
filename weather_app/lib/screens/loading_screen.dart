b9f0f0fae766d3efd46b5df3c72c5aa9bd682809
hankyul <rlagksruf16@gmail.com>
2020-01-29 23:55:57 +0900

    add exception & async to the gps system in weather app

--- weather_app/lib/screens/loading_screen.dart
+++ weather_app/lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/screens/location_geolocation.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,24 +8,24 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  void getLocation() async {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
 
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
 
   } 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
     );
   }
+
 }
