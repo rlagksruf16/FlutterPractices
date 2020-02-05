7ecb890d68743f9217cfa34805dc7c39305acf47
hankyul <rlagksruf16@gmail.com>
2020-02-05 23:39:11 +0900

    refactor chat service

--- chat_service/lib/screens/registration_screen.dart
+++ chat_service/lib/screens/registration_screen.dart
@@ -1,4 +1,7 @@
+import 'package:chat_service/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:chat_service/components/roundedButton.dart';
+import 'package:chat_service/constants.dart';
 
 class RegistrationScreen extends StatefulWidget {
 
@@ -33,22 +36,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration: kTextFieldDecoration.copyWith(hintText: '이메일을 입력하세요'),
             ),
             SizedBox(
               height: 8.0,
@@ -57,45 +45,12 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration: kTextFieldDecoration.copyWith(hintText: '비밀번호를 입력하세요'),
             ),
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement registration functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(color: Colors.blueAccent, title: '회원가입', onPressed: null),
           ],
         ),
       ),
--- chat_service/lib/screens/welcome_screen.dart
+++ chat_service/lib/screens/welcome_screen.dart
@@ -3,25 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:chat_service/screens/registration_screen.dart';
 import 'package:chat_service/main.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:chat_service/components/roundedButton.dart';
 
 class WelcomeScreen extends StatefulWidget {
-  
   static String id = 'welcome_screen';
-  
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
-
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
   AnimationController controller;
   Animation animation;
   @override
   void initState() {
     super.initState();
 
-    controller = AnimationController( duration: Duration(seconds: 3), vsync: this);
-    animation = ColorTween(begin: Colors.blueGrey, end: Colors.lightBlue).animate(controller);
+    controller =
+        AnimationController(duration: Duration(seconds: 3), vsync: this);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.lightBlue)
+        .animate(controller);
 
     controller.forward();
 
@@ -32,11 +34,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
     //   else if(status == AnimationStatus.dismissed)
     //     controller.forward();
     // });
-
-
     controller.addListener(() {
-      setState(() {
-      });
+      setState(() {});
     });
   }
 
@@ -45,7 +44,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
     controller.dispose();
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -78,45 +76,16 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(title: '로그인', color: Colors.lightBlueAccent, onPressed: (){
+              Navigator.pushNamed(context, LoginScreen.id);
+            },),
+            RoundedButton(title: '회원가입', color: Colors.blueAccent, onPressed: (){
+              Navigator.pushNamed(context, RegistrationScreen.id);
+            },),
           ],
         ),
       ),
     );
   }
 }
+
